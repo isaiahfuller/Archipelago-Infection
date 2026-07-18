1a464a18313b454708aaaf68c9d59f8cc4ec13b7
fuller <9921699+isaiahfuller@users.noreply.github.com>
2026-07-18 15:15:52 -0400

    refactor: update ItemUnion type definition to include additional item categories

--- worlds/dothack/data/Items.py
+++ worlds/dothack/data/Items.py
@@ -351,8 +351,10 @@ for v_data in VOLUME_DATA.values():
         if item not in ServerItems:
             ServerItems.append(item)
 
-ItemUnion = ConsumableItem | WeaponItem | InfectionWordListItem | PartyMemberItem | RyuBookItem \
+ItemUnion = (
+    ConsumableItem | WeaponItem | InfectionWordListItem | PartyMemberItem | RyuBookItem
     | ServerItem | VirusCoreItem | ArmorItem | InfectionLevelItem | GruntyFoodItem
+)
 
 ITEMS_MASTER: list[ItemUnion] = [
     *PartyMemberItems,
